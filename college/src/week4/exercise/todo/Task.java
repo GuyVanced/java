47b92f570844f2fd74f2b157d3d472faf7be9d3f
GuyVanced <pandeykushal017@gmail.com>
2023-08-26 14:25:51 +0545

    wrote code for question 3 of the ToDoList exercise

--- college/src/week4/exercise/todo/Task.java
+++ college/src/week4/exercise/todo/Task.java
@@ -5,19 +5,40 @@ public class Task {
      String taskName;
      String priority;
      LocalDate dueDate;
+     int priorityLevel;
 
     public Task(String taskName, String priority){
         this.taskName = taskName;
         this.priority = priority;
     }
+    public Task(String taskName, int priorityLevel){
+        this.taskName = taskName;
+        this.priorityLevel = priorityLevel;
+        setPriority(priorityLevel);
+    }
 
-//    public Task(String taskName, LocalDate dueDate ){
-//        this.taskName = taskName;
-//        this.dueDate = dueDate;
-//    }
+    public void setPriority(int priorityLevel) {
+        if(priorityLevel>0 && priorityLevel<=3){
+            this.priority = "Low";
+        }
+        else if (priorityLevel > 3 && priorityLevel<=7){
+            this.priority = "Medium";
+        }
+        else if (priorityLevel>7 && priorityLevel <=10){
+            this.priority = "High";
+        }
+        else{
+            System.out.println("Invalid priority level");
+        }
+    }
+
+    public Task(String taskName, LocalDate dueDate ){
+        this.taskName = taskName;
+        this.dueDate = dueDate;
+    }
 
     @Override
     public String toString() {
-        return "---------------\n" + "Task : " + taskName + "\nPriority : " + priority;
+        return "---------------\n" + "Task : " + taskName + (priority!= null ? ("\nPriority : " + priority) : ("\nDue Date : " + dueDate)) ;
     }
 }
--- college/src/week4/exercise/todo/ToDoList.java
+++ college/src/week4/exercise/todo/ToDoList.java
@@ -1,29 +1,41 @@
 package week4.exercise.todo;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class ToDoList {
 
-
     LinkedList<Task> link = new LinkedList<>();
-    Question1 question1 = new Question1();
+    LinkedTodo linkedTodo = new LinkedTodo();
+    ArrayTodo arrayTodo = new ArrayTodo();
 
     public static void main(String[] args) {
-        ToDoList todo = new ToDoList();
-        todo.question1.init();
-
+        ToDoList todolist = new ToDoList();
+        todolist.arrayTodo.init();
+        todolist.linkedTodo.init();
     }
 
-    class Question1 {
+    class LinkedTodo {
 
         public void init() {
-            link.add(new Task("Wash the dishes", "High"));
-            link.add(new Task("Clean your room", "High"));
-            link.add(new Task("Learn Python", "Low"));
-            link.add(new Task("Complete the assignment", "Medium"));
-            display();
-            removeLow(link);
-            display();
-            displayMedium();
+            link.add(new Task("Wash the dishes",2));
+            link.add(new Task("Clean your room", 3));
+            link.add(new Task("Learn Python", 8));
+            link.add(new Task("Complete the assignment", 9));
+            displayEvenPriority();
+        }
+
+        public void displayEvenPriority() {
+            Iterator<Task> iterator1 = link.iterator();
+            while (iterator1.hasNext()) {
+                Task task = iterator1.next();
+                if (task.priorityLevel % 2 == 0) {
+                    System.out.println(task.taskName);
+                }
+            }
         }
 
         public void display() {
@@ -45,7 +57,55 @@ public class ToDoList {
         public void removeLow(LinkedList<Task> link) {
             link.removeIf(task -> task.priority.equals("Low"));
         }
+
+
     }
+
+    class ArrayTodo{
+
+        ArrayList<Task> list = new ArrayList<Task>();
+        public void init() {
+            list.add(new Task("Complete report", LocalDate.of(2023, 12, 26)));
+            list.add(new Task("Call client", LocalDate.of(2023, 10, 28)));
+            list.add(new Task("Pay Bills", LocalDate.of(2023, 7, 20)));
+
+            list.get(2).dueDate = LocalDate.of(2024, 1, 2);
+
+            displayEarliestTask();
+        }
+
+        public void withinTwoDays(){
+            Iterator<Task> iterator = list.iterator();
+
+            while(iterator.hasNext()){
+                Task task = iterator.next();
+                Period period = Period.between(LocalDate.now(), task.dueDate);
+
+                if(period.getDays()<=2){
+                    System.out.println(task);
+                }
+            }
+        }
+
+        public void displayEarliestTask(){
+            Iterator<Task> iterator = list.iterator();
+            Task task = iterator.next();
+            LocalDate earliest = task.dueDate;
+            Task earliestTask = task;
+
+            while(iterator.hasNext()){
+                task = iterator.next();
+                if(task.dueDate.isBefore(earliest)){
+                    earliest = task.dueDate;
+                    earliestTask = task;
+                }
+            }
+            System.out.println(earliestTask);
+        }
+
+    }
+
+
 }
 
 
